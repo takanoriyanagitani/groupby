d70a8060f97690ae2a7b7d5adc6f27eb53f8770b
yanagitani <takanori.yanagitani@gmail.com>
2019-12-15 15:41:46 +0900

    trying to 'groupby' using go...

--- groupby-go/groupby.go
+++ groupby-go/groupby.go
@@ -19,3 +19,23 @@ func (j *JsonlScanner) Map() (map[string]interface{}, error) {
   e := json.Unmarshal(b, &m)
   return m, e
 }
+
+type Key interface {
+  Equals(other Key)
+}
+
+type StringKey struct { s string }
+
+func NewStringKey(s string) *StringKey { return &StringKey{ s: s } }
+
+func (a *StringKey) Equals(b *StringKey) bool { return a.s == b.s }
+
+type MapKey struct { m map[string]Key }
+
+func (a *MapKey) Equals(b *MapKey) bool {
+  switch len(a.m) != len(b.m) { case true: return false }
+  for key, value := range a.m {
+    switch value != b.m[key] { case true: return false }
+  }
+  return true
+}
--- groupby-go/groupby_test.go
+++ groupby-go/groupby_test.go
@@ -7,19 +7,9 @@ import (
   "bufio"
 )
 
-func TestScan(t *testing.T){
-  //tdir := "./.test/GroupBy"
-  //e1 := os.MkdirAll(tdir, 0755)
-  //switch e1 {
-  //  case nil: break
-  //  default:  t.Errorf("Unable to create test directory: %v\n", e1)
-  //}
-
+func TestMap(t *testing.T){
   f, e2 := os.Open("../testdata/t1.jsonl")
-  switch e2 {
-    case nil: break
-    default:  t.Errorf("Unable to open test file: %v\n", e2)
-  }
+  switch nil != e2 { case true:  t.Errorf("Unable to open test file: %v\n", e2) }
   defer f.Close()
 
   s := bufio.NewScanner(f)
@@ -28,10 +18,7 @@ func TestScan(t *testing.T){
 
   switch js.Scan() { case false: t.Errorf("Unable to get next data.\n") }
   m, e := js.Map()
-  switch e {
-    case nil: break
-    default:  t.Errorf("Unable to get map: %v\n", e)
-  }
+  switch nil != e { case true:  t.Errorf("Unable to get map: %v\n", e) }
   switch "google" == m["key"] { case false: t.Errorf("key mismatch.\n") }
   for js.Scan() {
     m, e = js.Map()
@@ -53,4 +40,12 @@ func TestScan(t *testing.T){
     case nil: t.Errorf("No parse error.\n")
     default:  t.Logf("Expected error: %v\n", e)
   }
+}
+
+func TestEquals(t *testing.T){
+  a := NewStringKey("apple")
+  b := NewStringKey("amazon")
+  c := NewStringKey("apple")
+  switch a.Equals(b){ case  true: t.Errorf("Not equal.\n") }
+  switch c.Equals(a){ case false: t.Errorf("Not differ.\n") }
 }
